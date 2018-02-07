81a93fcc5ea801dd5b7fabf75383c0d60b1ca387
Andrey Bychkovsky <bav0813@gmail.com>
2018-02-07 22:12:23 +0200

    first commit

---
+++ 23-24.php
@@ -0,0 +1,21 @@
+<?php
+
+
+    //23. Напишите 3 строки, используя три разные функции для вывода текста.
+
+
+$text1="1.Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br>";
+$text2="2.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type specimen book. .<br>";
+$text3="3.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. <br>";
+
+echo $text1;
+print_r($text2);
+var_export($text3);
+
+    // 24. Создайте в файле 3 комментария тремя разными типами.
+
+    //comment1
+    /*comment 2 begin
+    comment 2 end */
+    #comment3
---
+++ 25.php
@@ -0,0 +1,6 @@
+<?php
+
+
+    //25. Приведите пример, чем отличается <?php от <?=.
+
+    /*  <?php echo 'Hello'; ?>  is equal to <?='Hello, world!'; ?> */
